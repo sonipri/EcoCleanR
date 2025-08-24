092ec11d62db8b57c8de70095cf7003380aa0404
Priyanka Soni <sonip@usc.edu>
2025-08-24 16:07:30 -0700

    update 24th Aug

--- R/ec_extract_env_layers.R
+++ R/ec_extract_env_layers.R
@@ -5,8 +5,8 @@
 #' @param env_layers make a list of enviornmental layers which need to be extracted, example :BO_sstmean, BO_sstmax, BO_sstmin, BO_chomean, BO_phosphate or marspec layer, must check list_layer to know exact name of the layer code.
 #' @return a data table which has unique coordinates and env predictors
 #' @export
-#' @importFrom sdmpredictors load_layers
 #' @importFrom sdmpredictors list_layers
+#' @importFrom sdmpredictors load_layers
 #' @importFrom terra extract
 #'
 #' @examples
@@ -24,11 +24,11 @@ ec_extract_env_layers <- function(data, env_layers = env_layers) {
   data_x <- unique(data[, c("decimalLatitude", "decimalLongitude")])
   coordinates <- data_x[, c("decimalLongitude", "decimalLatitude")]
 
-  available_layers <- list_layers() # returns something like c("BO_sstmean", "BO_sstmax", ...)
+  available_layers <- sdmpredictors::list_layers() # returns something like c("BO_sstmean", "BO_sstmax", ...)
   for (layer_name in env_layers) {
     if (layer_name %in% available_layers$layer_code) {
       message("Extracting ", layer_name, "...")
-      layer_data <- load_layers(layer_name, rasterstack = TRUE)
+      layer_data <- sdmpredictors::load_layers(layer_name, rasterstack = TRUE)
       extracted_values <- terra::extract(layer_data, coordinates)
       data_x[[layer_name]] <- extracted_values
     } else {
